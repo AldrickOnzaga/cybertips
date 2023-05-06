b9281c643bcc03afa73ee33c59f4a5fdea01068c
Aldrick Onzaga <aonzaga.k11720977@umak.edu.ph>
2023-05-06 09:41:34 +0800

    new

---
+++ learning_materials.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CYBERTIPS</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/video.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  </head>
+  <body>
+    <div class="navigator">
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <img id="logo" class="logo" src="resources/cyb6.png" alt="CYBERTIPS Logo" />
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </div>
+
+    <div class="main">
+        <div class="modules-intro">
+            <h1>Learning Modules<h1>
+            <p>You can learn about cybersecurity and cyberawareness here</p>
+        </div>
+        <div class="modules-container">
+            <div class="module-details">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+      <h>CYBERTIPS</h>
+    </div>
+  </body>
+</html>
