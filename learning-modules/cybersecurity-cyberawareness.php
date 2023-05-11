9b9e754422e1a3198db80bb0898e35e70f82db70
Aldrick Onzaga <aonzaga.k11720977@umak.edu.ph>
2023-05-11 14:25:37 +0800

    new

---
+++ learning-modules/cybersecurity-cyberawareness.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CYBERTIPS</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/video.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  </head>
+  <body>
+    <div class="navigator">
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <img id="logo" class="logo" src="../resources/cyb6.png" alt="CYBERTIPS Logo" />
+      <ul>
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="../about.php">About</a></li>
+        <li><a href="../contact.php">Contact</a></li>
+      </ul>
+    </div>
+    <div class="main">
+      <div class="return">
+        <div class="return-button">
+          <a href="../learning_materials.php">Return</a>
+        </div>
+      </div>
+      <div class="next-prev-container">
+        <div class="prev-button">
+        </div>
+        <div class="next-button">
+          <a href="#">Next</a>
+        </div>
+      </div>
+      <div class="learning-modules-container">
+        <h1>Cybersecurity and Cyber Awareness</h1>
+      </div>
+    </div>
+
+    <div class="footer">
+      <h>CYBERTIPS</h>
+    </div>
+  </body>
+</html>
