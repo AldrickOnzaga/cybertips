af0f03953b9b1193092bafa1279b0f761352a60f
Aldrick Onzaga <aonzaga.k11720977@umak.edu.ph>
2023-05-11 17:01:36 +0800

    modified

--- learning-modules/cybersecurity-cyberawareness.php
+++ learning-modules/cybersecurity-cyberawareness.php
@@ -35,7 +35,38 @@
         </div>
       </div>
       <div class="learning-modules-container">
-        <h1>Cybersecurity and Cyber Awareness</h1>
+        <div class="module-title">
+          <h1>Cybersecurity and Cyber Awareness</h1>
+        </div>
+        <div class="module-body">
+          <div class="module-img">
+            <img src="../resources/cyber-awareness.png" alt="module-img" />
+          </div>
+          <p>Cyberawareness is the ability to recognize the risks and hazards posed by cyberattacks and to know how to defend oneself and their digital assets from them. It covers knowledge of the most effective practices for safe online conduct, including using strong passwords, maintaining secure surfing routines, and knowing how to spot phishing scams and other types of cybercrime.
+          </p>
+          <p>
+          In other words, being aware of how dangerous the world of the internet is and knowing how to protect themselves from threats that they may encounter.
+          </p>
+          <div class="module-img">
+            <img src="../resources/cybersec.png" alt="module-img" />
+          </div>
+          <p>The method of protecting networks, computers, servers, mobile devices, electronic systems, and data from hostile intrusions is known as "cyber security."</p>
+          <p>The phrase can be broken down into a few basic categories and is used in a wide range of applications such as:</p>
+          <ul>
+            <li>Network security</li>
+            <li>applications security</li>
+            <li> Information security</li>
+            <li> Operational security</li>
+            <li> Disaster recovery and business continuity</li>
+            <li>  Mobile Security</li>
+          </ul>
+          <h2>
+            Conclusion
+          </h2>
+          <p>
+          Between cybersecurity and cyberawareness, they are both similar to protecting the device and data of a person or self from the danger that they may encounter from the internet or a malicious person.
+          </p>
+        </div>
       </div>
     </div>
 
