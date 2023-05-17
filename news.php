dc401e4b138cf73eab95ffa388b1e41c05265efa
Aldrick Onzaga <aonzaga.k11720977@umak.edu.ph>
2023-05-17 22:53:49 +0800

    modified

--- news.php
+++ news.php
@@ -1,3 +1,8 @@
+<?php
+
+@include_once 'config/config.php';
+
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CYBERTIPS</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/video.css">
+    <link rel="stylesheet" href="css/news-container.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
   </head>
   <body>
@@ -23,7 +28,9 @@
     </div>
 
     <div class="main">
-   
+      <div class="news-container">
+        
+      </div>
     </div>
 
     <div class="footer">
@@ -38,4 +45,32 @@
             });
         });
   </script>
+  <script>
+      function fetchNews() {
+        // Make an AJAX request to fetch the news from the server
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            var newsContainer = document.querySelector('.news-container');
+            newsContainer.innerHTML = xhr.responseText;
+          }
+        };
+        xhr.open('GET', 'config/fetch_news.php', true);
+        xhr.send();
+      }
+
+      // Call fetchNews initially to populate the news container
+      fetchNews();
+
+      // Refresh the news every 5 seconds (adjust as needed)
+      setInterval(fetchNews, 50000000);
+
+      // Redirect to index.php when the logo is clicked
+      window.addEventListener('load', function() {
+        var logo = document.getElementById('logo');
+        logo.addEventListener('click', function() {
+          window.location.href = 'index.php';
+        });
+      });
+    </script>
 </html>
