7fb31f8a62436ecef659a884fcc0586ed1676c02
Aldrick Onzaga <aonzaga.k11720977@umak.edu.ph>
2023-05-17 22:53:42 +0800

    new

---
+++ config/fetch_news.php
@@ -0,0 +1,35 @@
+<?php
+// Establish a database connection
+@include_once 'config.php';
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch news data from the database
+$sql = "SELECT title, content, created_at FROM news ORDER BY created_at DESC";
+$result = $conn->query($sql);
+
+// Display news data in the news-container
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $title = $row['title'];
+        $content = $row['content'];
+        $created_at = $row['created_at'];
+        $first_paragraph = substr($content, 0, strpos($content, "\n"));
+
+        echo '<div class="news-item">';
+        echo '<h2>' . $title . '</h2>';
+        echo '<p>' . $created_at . '</p>';
+        echo $first_paragraph;
+        echo '<br>';
+        echo '<a href="news-content.php" class="read-more">Read more</a>';
+        echo '</div>';
+
+    }
+} else {
+    echo 'No news found.';
+}
+
+$conn->close();
+?>
